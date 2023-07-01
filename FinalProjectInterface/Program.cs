e2aace4c34d70d50c9ca329a6dda69ac293dca35
Name <email@exaple.ru>
2023-07-01 06:31:54 +0300

    Задание 1.

--- FinalProjectInterface/Program.cs
+++ FinalProjectInterface/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Введите 1 число: ");
+                int.TryParse(Console.ReadLine(), out int num1);
+                Console.WriteLine("Введите 2 число: ");
+                int.TryParse(Console.ReadLine(), out int num2);
+                Calculator calculator = new Calculator();
+                ((ISumNum)calculator).Sum(num1, num2);
+            }
+            catch { }
+        }
+        interface ISumNum
+        {
+            int Sum(int x, int y);
+        }
+        class Calculator : ISumNum
+        {
+            int ISumNum.Sum(int x, int y) { return x + y; }
         }
     }
 }
